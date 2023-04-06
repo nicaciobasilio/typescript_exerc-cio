0a38e4a49c0f54fe34fac81210f209a206f4931b
Nicacio Basilio <nicacioufrn@gmail.com>
2023-04-06 00:23:38 -0300

    Finalização do exercício

---
+++ exercicio.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplica(numeroUm, numeroDois) {
+    return numeroUm * numeroDois;
+}
+const resultado = multiplica(3, 3);
+console.log(resultado);
+function dizNome(nome) {
+    return `Olá ${nome}`;
+}
+const apresentacao = dizNome("Nicacio");
+console.log(apresentacao);
---
+++ exercicio.ts
@@ -0,0 +1,13 @@
+function multiplica(numeroUm: number, numeroDois: number) {
+    return numeroUm * numeroDois;
+}
+
+const resultado = multiplica(3, 3);
+console.log(resultado);
+
+function dizNome(nome: string) {
+    return `Olá ${nome}`
+}
+
+const apresentacao = dizNome("Nicacio");
+console.log(apresentacao);
